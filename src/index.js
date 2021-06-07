4c691cbc7b2e7f03674d43ceddda055807dc1e29
Kevin Brown <kevin.brown@techin.site>
2021-06-07 10:56:44 +1000

    Make vite config patch compatible with both types of alias definitions.

--- src/index.js
+++ src/index.js
@@ -14,12 +14,29 @@ module.exports = () => ({
     //
     // I agree, and we should be working to actually fix React PDF
     // but this shim keeps us working as we do.
-    config.resolve.alias = {
-      ...config.resolve.alias,
+    if (Array.isArray(config.resolve.alias)) {
+      config.resolve.alias.push(
+        {
+          find: "process",
+          replacement: "process/browser",
+        },
+        {
+          find: "stream",
+          replacement: "vite-compatible-readable-stream",
+        },
+        {
+          find: "zlib",
+          replacement: "browserify-zlib",
+        }
+      );
+    } else {
+      config.resolve.alias = {
+        ...config.resolve.alias,
 
-      process: "process/browser",
-      stream: "vite-compatible-readable-stream",
-      zlib: "browserify-zlib",
-    };
+        process: "process/browser",
+        stream: "vite-compatible-readable-stream",
+        zlib: "browserify-zlib",
+      };
+    }
   },
 });
